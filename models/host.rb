226a938fef5e7a88ed69211fa667987a765c03cf
Wesley Dawson <wdawson@mozilla.com>
2011-05-25 15:35:19 -0700

    remove some old methods from host.rb

--- models/host.rb
+++ models/host.rb
@@ -23,20 +23,5 @@ module Dash::Models
       return @params['cluster']
     end
 
-    def self.find_by_name_and_cluster(name, cluster)
-      Host.each do |host_name, host|
-        next unless host_name == name
-        return host if host.cluster == cluster
-      end
-      return nil
-    end
-
-    def self.find_by_cluster(cluster)
-      ret = []
-      Host.each do |name, host|
-        ret << host if host.cluster == cluster
-      end
-      return ret
-    end
   end # Dash::Models::Host
 end # Dash::Models
