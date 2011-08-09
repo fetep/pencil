7adde3a52d7673535834efdc83e07c47e772eea1
Wesley Dawson <wdawson@mozilla.com>
2011-08-09 15:33:34 -0700

    Herp derp

--- lib/models/host.rb
+++ lib/models/host.rb
@@ -71,7 +71,7 @@ module Dash::Models
 
     def self.find_by_name_and_cluster(name, cluster)
       Host.each do |host_name, host|
-        next unless host_name = name
+        next unless host_name == name
         return host if host.cluster == cluster
       end
       return nil
